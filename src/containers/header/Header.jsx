e2b09c551e4a09ab7187f1f5aec31fcb3e5ff982
NicolayOlsen <nicolay2003.nao@gmail.com>
2022-12-08 22:53:29 +0100

    first commit

---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./header.css"
+
+const Header = () => {
+  return (
+    <div className='parallax__header section__padding' id="home">
+        <div className="parallax__header-content">
+            <h1 className='gradient__text'>Achieve mastery through challenge</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Header;
