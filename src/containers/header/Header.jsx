6db2306ffc926af7fc57c0d65c64bbb77cad8dec
NicolayOlsen <nicolay.andre.olsen@udir.no>
2022-12-09 14:30:42 +0100

    update

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import "./header.css"
+import python from "../../assets/python.svg"
+import js from "../../assets/js.svg"
+
 
 const Header = () => {
   return (
-    <div className='parallax__header section__padding' id="home">
-        <div className="parallax__header-content">
-            <h1 className='gradient__text'>Achieve mastery through challenge</h1>
-        </div>
+    <div className="parallax__header" id="home">
+      <div className="parallax__header-content">
+        <img src={python} data-value="-5" alt="python" />
+        <img src={js} data-value="5" alt="javascript" />
+        <img src={python} data-value="-2" alt="python" />
+        <img src={python} data-value="6" alt="python" />
+        <img src={python} data-value="4" alt="python" />
+        <img src={python} data-value="-6" alt="python" />
+        <img src={python} data-value="8" alt="python" />
+        <img src={python} data-value="-4" alt="python" />
+        <img src={python} data-value="5" alt="python" />
+        <img src={python} data-value="-9" alt="python" />
+        <img src={python} data-value="-5" alt="python" />
+        <h1 className="gradient__text" data-value="3" >Parallax</h1>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Header;
